19bc9b0499f2eadf9937682ecabdb43b2f7277a9
rosariodm20 <rosariodelgadomoreno17@gmail.com>
2023-11-27 15:09:46 +0100

    canela en rama

--- Proyecto3_3/src/Clase3_3.java
+++ Proyecto3_3/src/Clase3_3.java
@@ -4,6 +4,7 @@ public class Clase3_3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Proyecto 3_3 Rosario Delgado Moreno");
+		System.out.println("Canela en rama");
 	}
 
 }
